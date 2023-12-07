dedd3cade52122418d87f46d24b71fa57fc7f051
Matteo Terrien <matteo.terrien@gmail.com>
2023-12-06 18:44:08 -0800

    valuesToSQL

--- multiMediaProject/src/Main.java
+++ multiMediaProject/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         valuesToSQL values = new valuesToSQL();
-        values.getSongValues();
+        values.sendSongValues();
     }
 }
--- multiMediaProject/src/valuesToSQL.java
+++ multiMediaProject/src/valuesToSQL.java
@@ -3,37 +3,99 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class valuesToSQL {
 
-    public Songs songs = new Songs();
-    public Artists artists = new Artists();
-    public Albums albums = new Albums();
-    public Playlists playlists = new Playlists();
 
-    public void getSongValues() throws SQLException {
-        String str = "";
-        for (int i = 0; i < songs.getSongs().size(); i++) {
-            Songs.Song song = songs.songs.get(i);
-            str += ("(" + song.getSongID() + ", "
-                    + song.getName() + ", "
-                    + song.getLength() + ", "
-                    + song.getDate() + ")");
-            if (i < songs.getSongs().size() - 1)
-                str += ", ";
-        }
-        Connection connection; // Initialize and manage your database connection
-        try {
-            // Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/musicGroup?user=musicGroup&password=musicCSC365!");
+   public Songs songs = new Songs();
+   public Artists artists = new Artists();
+   public Albums albums = new Albums();
+   public Playlists playlists = new Playlists();
 
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("Insert into songs values" + str);
-            connection.commit();
-            connection.close();
-        } catch (Exception e) {
-            // might need to catch other exceptions found in slides code
-            e.printStackTrace();
-        }
-    }
+
+   public void sendSongValues() throws SQLException {
+       String str = "";
+       for (int i = 0; i < songs.getSongs().size(); i++) {
+           Songs.Song song = songs.songs.get(i);
+           str += ("(" + song.getSongID() + ", "
+                   + song.getName() + ", "
+                   + song.getLength() + ", "
+                   + song.getDate() + ")");
+           if (i < songs.getSongs().size() - 1)
+               str += ", ";
+       }
+       Connection connection; // Initialize and manage your database connection
+       try {
+           // Class.forName("com.mysql.jdbc.Driver");
+           connection = DriverManager.getConnection(
+                   "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/musicGroup?user=musicGroup&password=musicCSC365!");
+
+
+           Statement statement = connection.createStatement();
+           statement.executeUpdate("Insert into Songs (songID, name, length, date) values " + str);
+           connection.commit();
+           connection.close();
+       } catch (Exception e) {
+           // might need to catch other exceptions found in slides code
+           e.printStackTrace();
+       }
+   }
+
+
+   public void sendArtistValues() throws SQLException {
+       String str = "";
+       for (int i = 0; i < artists.getArtists().size(); i++) {
+           Artists.Artist artist = artists.artists.get(i);
+           str += ("(" + artist.getArtistID() + ", "
+                   + artist.getName() + ")");
+           if (i < artists.getArtists().size() - 1)
+               str += ", ";
+       }
+       Connection connection; // Initialize and manage your database connection
+       try {
+           // Class.forName("com.mysql.jdbc.Driver");
+           connection = DriverManager.getConnection(
+                   "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/musicGroup?user=musicGroup&password=musicCSC365!");
+
+
+           Statement statement = connection.createStatement();
+           statement.executeUpdate("Insert into Artists (artistID, name) values " + str);
+           connection.commit();
+           connection.close();
+       } catch (Exception e) {
+           // might need to catch other exceptions found in slides code
+           e.printStackTrace();
+       }
+   }
+
+
+   public void sendAlbumValues() throws SQLException {
+       String str = "";
+       for (int i = 0; i < albums.getAlbums().size(); i++) {
+           Albums.Album album = albums.albums.get(i);
+           str += ("(" + album.getAlbumID() + ", "
+                   + album.getArtistID() + ", "
+                   + album.getReleaseDate() + ", "
+                   + album.getName() + ", "
+                   + album.getGenre() + ")");
+           if (i < albums.getAlbums().size() - 1)
+               str += ", ";
+       }
+       Connection connection; // Initialize and manage your database connection
+       try {
+           // Class.forName("com.mysql.jdbc.Driver");
+           connection = DriverManager.getConnection(
+                   "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/musicGroup?user=musicGroup&password=musicCSC365!");
+
+
+           Statement statement = connection.createStatement();
+           statement.executeUpdate("Insert into Albums (albumID, artistID, releaseDate, name, genre) values " + str);
+           connection.commit();
+           connection.close();
+       } catch (Exception e) {
+           // might need to catch other exceptions found in slides code
+           e.printStackTrace();
+       }
+   }
 }
+
