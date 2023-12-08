9d8dabacc6fafb9dfc5aea68726683c40efc24c3
Matteo Terrien <matteo.terrien@gmail.com>
2023-12-07 22:11:13 -0800

    WLKRFG

--- multiMediaProject/src/Main.java
+++ multiMediaProject/src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         valuesToSQL values = new valuesToSQL();
         // values.sendSongValues();
         // values.sendArtistValues();
-        values.sendSongToArtist();
-        // values.sendAlbumValues();
-        // values.sendAlbumSongsValues();
-        // values.sendPaylistValues();
-        // values.sendPlaylistSongsValues();
+        //
+        values.sendAlbumValues();
+        values.sendAlbumSongsValues();
+        values.sendPaylistValues();
+        values.sendPlaylistSongsValues();
     }
 }
